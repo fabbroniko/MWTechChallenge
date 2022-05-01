342379f75231ab92964d3faf1880811fd01f5a93
Nicola Fabbrini <fabbroniko@hotmail.it>
2022-05-01 17:15:11 +1000

    PASS - Addition operation
    Working addition functionality

--- src/main/java/org/nicolafabbrini/calculator/Operation.java
+++ src/main/java/org/nicolafabbrini/calculator/Operation.java
@@ -14,10 +14,17 @@ public class Operation {
     public double getResult() {
         Optional.ofNullable(expression).orElseThrow(() -> new IllegalArgumentException("The expression must be set"));
 
-        if(!Pattern.matches("([0-9]+([.]?[0-9]+)?)\\s+[\\+]{1}\\s+([0-9]+([.]?[0-9]+)?)", expression)) {
+        if(!Pattern.matches("([0-9]+([.]?[0-9]+)?)\\s{1}[\\+]{1}\\s{1}([0-9]+([.]?[0-9]+)?)", expression)) {
             throw new IllegalArgumentException("The expression is not valid");
         }
 
-        return -1;
+        String[] sOperands = expression.split("\\s{1}[\\+]{1}\\s{1}");
+        double x = Double.parseDouble(sOperands[0]);
+        double y = Double.parseDouble(sOperands[1]);
+
+        // Truncate digits to avoid floating point errors in the result
+        double scale = Math.pow(10, 3);
+
+        return Math.round((x + y)*scale)/scale;
     }
 }
