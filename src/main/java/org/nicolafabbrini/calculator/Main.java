4b665c98b66dd760d84eef5c1741e527ed7f991f
Nicola Fabbrini <fabbroniko@hotmail.it>
2022-05-02 09:20:40 +1000

    User input from program's args
    Upon reading the requirements, it seems like the user input must be
    taken from program's argument rather than looping around indefinitely
    until the user types \q.

--- src/main/java/org/nicolafabbrini/calculator/Main.java
+++ src/main/java/org/nicolafabbrini/calculator/Main.java
@@ -1,71 +1,24 @@
 package org.nicolafabbrini.calculator;
 
 import java.text.MessageFormat;
-import java.util.Scanner;
 
 public class Main {
 
-    private static final String QUIT_COMMAND = "\\q";
-
     public static void main(final String[] args) {
-        intro();
-
         final Calculator calculator = new Calculator();
-        final Scanner scanner = new Scanner(System.in);
 
-        // Infinite loop until user types \q
-        String expression;
-        do {
-            System.out.print("Type your expression: ");
-            expression = scanner.nextLine();
+        // Sets the expression from the first *Program Argument* if available.
+        String expression = null;
+        if(args.length > 0) {
+            expression = args[0];
+        }
 
-            // Skip to verify the while statement and leave the loop
-            if(QUIT_COMMAND.equals(expression)) {
-                continue;
-            }
-
-            // Performs the operation
-            try {
-                final double result = calculator.getResult(expression);
-                System.out.println(MessageFormat.format("Result of {0} is {1}", expression, result));
-            } catch (final Exception e) {
-                System.out.println(e.getMessage());
-            }
-        } while (!QUIT_COMMAND.equals(expression));
-    }
-
-    /**
-     * Shows instructions on how to use the calculator.
-     */
-    private static void intro() {
-        System.out.println("Program Author: Nicola Fabbrini");
-        System.out.println("ASCII Art Author: David Berner & Nicola Fabbrini");
-        System.out.println();
-        System.out.println("                   /\"\\             ");
-        System.out.println("  //  //       /\"\\|\\./|/\"\\      ");
-        System.out.println("  ||  ||      |\\./|   |\\./|        ");
-        System.out.println("  ||  ||      |   |   |   |          ");
-        System.out.println("              |   |>~<|   |/\"\\     ");
-        System.out.println("              |>~<|   |>~<|\\./|     ");
-        System.out.println("              |   |   |   |   |      ");
-        System.out.println("          /~T\\|   |   =[@]=   |     ");
-        System.out.println("          |_/ |   |   |   |   |      ");
-        System.out.println("          |   | ~   ~   ~ |   |      ||  ||");
-        System.out.println("          |~< |             ~ |      ||  ||");
-        System.out.println("          |   '               |      //  //");
-        System.out.println("          \\                   |     ");
-        System.out.println("           \\                 /      ");
-        System.out.println("            \\               /       ");
-        System.out.println("             \\.            /        ");
-        System.out.println("               |          |          ");
-        System.out.println("               |          |          ");
-        System.out.println();
-        System.out.println("Welcome to YourWave! Our newest calculator with anti-floating point error (cheat) system.");
-        System.out.println("This calculator is currently able to perform the following operations: ");
-        System.out.println("- Addition -> Syntax is {x} + {y}");
-        System.out.println("- Subtraction -> Syntax is {x} - {y}");
-        System.out.println("- Multiplication -> Syntax is {x} * {y}");
-        System.out.println("- Division -> Syntax is {x} / {y}");
-        System.out.println(MessageFormat.format("- Quit -> Syntax is {0}", QUIT_COMMAND));
+        // Tries to perform the calculation. Exceptions include infinity (0/0), undefined (5/0) and input validation errors.
+        try {
+            final double result = calculator.getResult(expression);
+            System.out.println(MessageFormat.format("Result of {0} is {1}", expression, result));
+        } catch (final Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
