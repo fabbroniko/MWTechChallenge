5576b9ff13aa4b60884d7d5165e0ecc60a15ab90
Nicola Fabbrini <fabbroniko@hotmail.it>
2022-05-01 15:34:38 +1000

    Adding requirements for a meaningful message when the expression is null
    Added a meaninful message that can be printed on the terminal when no
    expression is passed to the Operation class.

--- src/test/java/org/nicolafabbrini/calculator/OperationTest.java
+++ src/test/java/org/nicolafabbrini/calculator/OperationTest.java
@@ -1,6 +1,7 @@
 package org.nicolafabbrini.calculator;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,5 +11,13 @@ public class OperationTest {
     public void testOperationNullInput() {
         final Operation operation = new Operation(null);
         assertThrows(IllegalArgumentException.class, operation::getResult);
+    }
+
+    @Test
+    public void testOperationNullInputWithMessage() {
+        final Operation operation = new Operation(null);
+        Exception thrown = assertThrows(IllegalArgumentException.class, operation::getResult);
+
+        assertEquals("The expression must be set", thrown.getMessage());
     }
 }
