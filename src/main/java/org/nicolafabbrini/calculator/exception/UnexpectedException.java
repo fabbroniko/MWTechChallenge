61929739af90f85fc7beaec659a6bb58a47a3d0d
Nicola Fabbrini <fabbroniko@hotmail.it>
2022-05-03 21:47:35 +1000

    Comments and magic string
    Comments added to the new exceptions and removed a magic string from one
    of them.

--- src/main/java/org/nicolafabbrini/calculator/exception/UnexpectedException.java
+++ src/main/java/org/nicolafabbrini/calculator/exception/UnexpectedException.java
@@ -1,5 +1,8 @@
 package org.nicolafabbrini.calculator.exception;
 
+/**
+ * Indicates an unforeseen-unforeseen (unforeseen^2) exception, and it's likely to require the dev's attention.
+ */
 public class UnexpectedException extends RuntimeException {
 
     private static final String ERROR_MESSAGE = "Something went wrong. Please contact the author, we are currently unable to notify our devs automatically.";
